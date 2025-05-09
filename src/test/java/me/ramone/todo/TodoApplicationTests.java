8cf20c2b8aa631786cd11b39b5f97474b4d01c90
Ramone Robertson <ramonerobertson10@gmail.com>
2025-05-09 18:48:50 +0900

    Initial commit

---
+++ src/test/java/me/ramone/todo/TodoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ramone.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
